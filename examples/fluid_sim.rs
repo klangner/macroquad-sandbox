bfc2829d096667a6f5e0277edddef9c51751491a
klangner <klangner@gmail.com>
2022-02-24 23:11:49 +0100

    diffuse implemented

--- examples/fluid_sim.rs
+++ examples/fluid_sim.rs
@@ -2,14 +2,13 @@
 //
 
 use macroquad::prelude::*;
-use macroquad_sandbox::quad::*;
 use macroquad_sandbox::fluid::*;
 
 
-const WINDOW_WIDTH: usize = 1024;
-const WINDOW_HEIGHT: usize = 800;
-const UNIVERSE_WIDTH: usize = 10;
-const UNIVERSE_HEIGHT: usize = 10;
+const WINDOW_WIDTH: i32 = 1024;
+const WINDOW_HEIGHT: i32 = 800;
+const UNIVERSE_WIDTH: i32 = 10;
+const UNIVERSE_HEIGHT: i32 = 10;
 
 
 fn plot_universe(universe: &Universe) {
@@ -18,16 +17,6 @@ fn plot_universe(universe: &Universe) {
 
     clear_background(BLACK);
 
-    // Grid
-    for i in 1..universe.width() {
-        let x = i as f32 * cell_dx + 5.;
-        draw_line(x, 0.0, x, screen_height(), 1.0, DARKGRAY)
-    }
-    for i in 1..universe.height() {
-        let y = i as f32 * cell_dy + 5.;
-        draw_line(0.0, y, screen_width(), y, 1.0, DARKGRAY)
-    }
-
     // Densities
     for x in 0..universe.width() {
         for y in 0..universe.height() {
@@ -35,7 +24,7 @@ fn plot_universe(universe: &Universe) {
             let cy = y as f32 * cell_dy;
             let intensity = universe.density_at(x, y);
             let color = Color::new(intensity, intensity, intensity, 1.00);
-            draw_rectangle(cx+1.0, cy+1.0, cell_dx-2.0, cell_dy-2.0, color);
+            draw_rectangle(cx, cy, cell_dx, cell_dy, color);
         }
     }
 
@@ -49,6 +38,16 @@ fn plot_universe(universe: &Universe) {
             draw_line(cx, cy, cx+v.x*cell_dx/2.0, cy+v.y*cell_dy/2.0, 1.0, RED);
         }
     }
+    
+    // Grid
+    for i in 1..universe.width() {
+        let x = i as f32 * cell_dx;
+        draw_line(x, 0.0, x, screen_height(), 1.0, DARKGRAY)
+    }
+    for i in 1..universe.height() {
+        let y = i as f32 * cell_dy;
+        draw_line(0.0, y, screen_width(), y, 1.0, DARKGRAY)
+    }
 }
 
 
@@ -56,24 +55,39 @@ fn window_conf() -> Conf {
     Conf {
         window_title: "Fluid sim".to_owned(),
         fullscreen: false,
-        window_width: WINDOW_WIDTH as i32,
-        window_height: WINDOW_HEIGHT as i32,
+        window_width: WINDOW_WIDTH,
+        window_height: WINDOW_HEIGHT,
         ..Default::default()
     }
 }
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let field = Universe::new(UNIVERSE_WIDTH, UNIVERSE_HEIGHT, Vec2d::new(1., 0.), 0.);
+    let mut universe = Universe::new(UNIVERSE_WIDTH, UNIVERSE_HEIGHT, Vec2d::new(1., 0.), 0.);
+    let cell_dx = screen_width() / universe.width() as f32;
+    let cell_dy = screen_height() / universe.height() as f32;
 
     loop {
+        let dt = get_frame_time();
+        
+        universe.diffuse(dt);
+
         // Process input
         #[cfg(not(target_arch = "wasm32"))]
         if is_key_down(KeyCode::Q) | is_key_down(KeyCode::Escape) {
             break;
         }
 
-        plot_universe(&field);
+        // Process mouse
+        if is_mouse_button_down(MouseButton::Left) {
+            let (mouse_x, mouse_y) = mouse_position();
+            let x = f32::trunc(mouse_x / cell_dx) as usize;
+            let y = f32::trunc(mouse_y / cell_dy) as usize;
+            universe.increase_density(x as i32, y as i32, dt / 1.0);
+        }
+
+        // Plot
+        plot_universe(&universe);
         
         next_frame().await
     }
--- src/fluid.rs
+++ src/fluid.rs
@@ -7,10 +7,11 @@ pub struct Vec2d {
 }
 
 pub struct Universe {
-    width: usize,
-    height: usize,
+    width: i32,
+    height: i32,
     velocities: Vec<Vec2d>,
-    densities: Vec<f32>
+    densities: Vec<f32>,
+    diffusion_rate: f32,
 }
 
 
@@ -21,32 +22,67 @@ impl Vec2d {
 }
 
 impl Universe {
-    pub fn new(width: usize, height: usize, velocity: Vec2d, density: f32) -> Universe {
+    pub fn new(width: i32, height: i32, velocity: Vec2d, density: f32) -> Universe {
         Universe { 
             width,
             height,
-            velocities: vec![velocity; width*height], 
-            densities: vec![density; width*height],
+            velocities: vec![velocity; (width*height) as usize], 
+            densities: vec![density; (width*height) as usize],
+            diffusion_rate: 0.01,
         }
     }
 
-    pub fn width(&self) -> usize {
+    pub fn width(&self) -> i32 {
         self.width
     }
 
-    pub fn height(&self) -> usize {
+    pub fn height(&self) -> i32 {
         self.height
     }
 
-    pub fn density_at(&self, x: usize, y: usize) -> f32 {
+    pub fn density_at(&self, x: i32, y: i32) -> f32 {
+        if x < 0 || y < 0 || x >= self.width || y >= self.height {
+            return 0.0
+        }
        self.densities[self.xy_idx(x, y)] 
     }
 
-    pub fn velocity_at(&self, x: usize, y: usize) -> Vec2d {
+    pub fn increase_density(&mut self, x: i32, y: i32, amount: f32) {
+        let idx = self.xy_idx(x, y);
+        self.densities[idx] += amount;
+        if self.densities[idx] > 1.0 {
+            self.densities[idx] = 1.0;
+        }
+
+    }
+
+    pub fn velocity_at(&self, x: i32, y: i32) -> Vec2d {
        self.velocities[self.xy_idx(x, y)] 
     }
 
-    fn xy_idx(&self, x: usize, y: usize) -> usize {
-        y * self.width + x        
+    // Calculate diffusion using
+    // Gauss-Seidel relaxation method for numerical stability
+    pub fn diffuse(&mut self, dt: f32) {
+        let a = dt * self.diffusion_rate * (self.width * self.height) as f32;
+        let d0 = self.densities.clone();
+
+        for _ in 0..20 {
+            for x in 0..self.width as i32 {
+                for y in 0..self.height as i32 {
+                    let idx = self.xy_idx(x, y);
+                    self.densities[idx] = (
+                        d0[idx] + 
+                        a*(self.density_at(x-1,y) + 
+                        self.density_at(x+1,y) + 
+                        self.density_at(x,y-1) +
+                        self.density_at(x,y+1))
+                    )/(1.0 + 4.0 * a)
+               }
+            }
+        }
+    }
+
+    fn xy_idx(&self, x: i32, y: i32) -> usize {
+        (y * self.width + x) as usize
     }
 }
