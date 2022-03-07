c3c62af6cb4ac469f0c2c0d5effced7606748e17
klangner <klangner@gmail.com>
2022-03-07 21:57:19 +0100

    Perlin noise

--- examples/noise.rs
+++ examples/noise.rs
@@ -2,10 +2,12 @@
 //
 
 use macroquad::prelude::*;
+use macroquad::prelude::Color;
+use noise::{utils::*, Perlin, Seedable};
 
 
-const MAP_WIDTH: usize = 512;
-const MAP_HEIGHT: usize = 512;
+const MAP_WIDTH: u32 = 512;
+const MAP_HEIGHT: u32 = 512;
 
 
 fn draw_map() {
@@ -15,7 +17,7 @@ fn draw_map() {
 
 fn window_conf() -> Conf {
     Conf {
-        window_title: "Fluid sim".to_owned(),
+        window_title: "Noise map".to_owned(),
         fullscreen: false,
         window_width: MAP_WIDTH as i32,
         window_height: MAP_HEIGHT as i32,
@@ -25,7 +27,21 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let image = Image::gen_image_color(MAP_WIDTH as u16, MAP_HEIGHT as u16, RED);
+    let perlin = Perlin::default();
+    let perlin = perlin.set_seed(10);
+
+    let noise = PlaneMapBuilder::new(&perlin)
+        .set_size(MAP_WIDTH as usize, MAP_HEIGHT as usize)
+        .set_x_bounds(-5.0, 5.0)
+        .set_y_bounds(-5.0, 5.0)
+        .build();
+    let mut image = Image::gen_image_color(MAP_WIDTH as u16, MAP_HEIGHT as u16, BLACK);
+    for i in 0..MAP_WIDTH {
+        for j in 0..MAP_HEIGHT {
+            let c = noise.get_value(i as usize, j as usize) as f32;
+            image.set_pixel(i, j, Color::new(c, c, c, 1.));
+        }
+    }
     let texture = Texture2D::from_image(&image);
 
     loop {
