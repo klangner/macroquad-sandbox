37d47959b37005b9effa0d2893f63ab7773d1b71
klangner <klangner@gmail.com>
2025-05-14 22:23:18 +0200

    Flying

--- src/bin/gpst.rs
+++ src/bin/gpst.rs
@@ -1,6 +1,8 @@
 // GPS Triangle racing
 //
 
+use std::f32::consts::PI;
+
 use macroquad::{prelude::*, ui};
 use macroquad_sandbox::mqx::drawx;
 
@@ -15,22 +17,99 @@ const PANEL_COLOR: Color = color_u8!(0x62, 0xaa, 0xea, 255);
 
 
 struct Glider {
-    pos: Vec3,  // meters
-
+    // body: GliderBody,
+    pos: Vec2,  // meters
+    velocity: f32, // m/s
+    orientation: Vec2, // radians
+    altitude: f32,
+    hvelocity: f32, // m/s
 }
 
 impl Glider {
     pub fn new() -> Self {
         Self {
-            pos: vec3(0., 0. ,100.),
+            pos: vec2(150., 100.),
+            velocity: 10.,
+            orientation: vec2(0., 1.),
+            altitude: 100.,
+            hvelocity: -1.,
         }
     }
 
-    pub fn update(&mut self, _dt: f32) {
+    pub fn update(&mut self, dt: f32) {
+        self.pos +=  dt * self.velocity * self.orientation;
+        self.altitude += self.hvelocity * dt;
+    }
 
+    pub fn turn_left(&mut self, dt: f32) {
+        let rhs = Vec2::from_angle(-0.5 * dt);
+        self.orientation = self.orientation.rotate(rhs);
+    }
+
+    pub fn turn_right(&mut self, dt: f32) {
+        let rhs = Vec2::from_angle(0.5 * dt);
+        self.orientation = self.orientation.rotate(rhs);
     }
 }
 
+
+struct FlyingField {
+    area_size: Vec2,
+    tp1: Vec2,
+    tp2: Vec2,
+    tp3: Vec2,
+}
+
+impl FlyingField {
+    pub fn new() -> Self {
+        let area_size = vec2(700., 400.);
+        Self { 
+            area_size,
+            tp1: vec2(550., 300.),
+            tp2: vec2(350., 100.),
+            tp3: vec2(150., 300.),
+         }
+    }
+}
+
+struct MapView {
+    pos: Vec2,
+    size: Vec2,
+    flying_field: FlyingField,
+    glider_image: Texture2D,
+}
+
+impl MapView {
+    pub fn new(pos: Vec2, size: Vec2, flying_field: FlyingField, glider_image: Texture2D) -> Self {
+        Self { pos, size, flying_field, glider_image }
+    }
+
+    pub fn draw(&self, glider: &Glider) {
+        let area =  &self.flying_field.area_size;
+        self.draw_tp(&self.flying_field.tp1, area);
+        self.draw_tp(&self.flying_field.tp2, area);
+        self.draw_tp(&self.flying_field.tp3, area);
+
+        let params = DrawTextureParams {
+            rotation: glider.orientation.to_angle(),
+            ..Default::default()
+        };
+
+        let scale_x = self.size.x / area.x;
+        let scale_y = self.size.y / area.y;
+        let glider_x = glider.pos.x * scale_x;
+        let glider_y = glider.pos.y * scale_y;
+        draw_texture_ex(&self.glider_image, glider_x, glider_y, WHITE, params);
+    }
+
+    pub fn draw_tp(&self, tp: &Vec2, area: &Vec2) {
+        let scale_x = self.size.x / area.x;
+        let scale_y = self.size.y / area.y;
+        let x = self.pos.x + tp.x * scale_x;
+        let y = self.pos.y + tp.y * scale_y;
+        draw_circle(x, y, 10., ORANGE);
+    }
+}
 
 struct PositionPanel {
     pos: Vec2,
@@ -49,10 +128,10 @@ impl PositionPanel {
         
         ui::root_ui().label(
             vec2(self.pos.x + 10., self.pos.y + 10.), 
-            &format!("Position: {}m, {}m", glider.pos.x, glider.pos.y));
+            &format!("Position: {:.1} m, {:.1} m", glider.pos.x, glider.pos.y));
         ui::root_ui().label(
             vec2(self.pos.x + 10., self.pos.y + 10. + hoffset), 
-            &format!("Altitude: {}m", glider.pos.z));
+            &format!("Altitude: {:.1} m", glider.altitude));
     }
 }
 
@@ -66,20 +145,20 @@ impl OrientationPanel {
         Self { pos, size }
     }
 
-    fn draw(&self, _glider: &Glider) {
-        let hoffset = LABEL_FONT_SIZE as f32;
+    fn draw(&self, glider: &Glider) {
+        // let hoffset = LABEL_FONT_SIZE as f32;
 
         drawx::draw_rounded_rectangle(self.pos, self.size, 10., PANEL_COLOR); 
         
         ui::root_ui().label(
             vec2(self.pos.x + 10., self.pos.y + 10.), 
-            &format!("Yaw: {} deg", 0));
-        ui::root_ui().label(
-            vec2(self.pos.x + 10., self.pos.y + 10. + hoffset), 
-            &format!("Roll: {} deg", 0));
-        ui::root_ui().label(
-            vec2(self.pos.x + 10., self.pos.y + 10. + 2.*hoffset), 
-            &format!("Pitch: {} deg", 0));
+            &format!("Yaw: {:.2} deg", glider.orientation.to_angle() * 180. / PI));
+        // ui::root_ui().label(
+        //     vec2(self.pos.x + 10., self.pos.y + 10. + hoffset), 
+        //     &format!("Roll: {} deg", glider.orientation.y));
+        // ui::root_ui().label(
+        //     vec2(self.pos.x + 10., self.pos.y + 10. + 2.*hoffset), 
+        //     &format!("Pitch: {} deg", glider.orientation.z));
     }
 }
 
@@ -93,36 +172,17 @@ impl VelocityPanel {
         Self { pos, size }
     }
 
-    fn draw(&self, _glider: &Glider) {
+    fn draw(&self, glider: &Glider) {
         let hoffset = LABEL_FONT_SIZE as f32;
 
         drawx::draw_rounded_rectangle(self.pos, self.size, 10., PANEL_COLOR); 
         
         ui::root_ui().label(
             vec2(self.pos.x + 10., self.pos.y + 10.), 
-            &format!("vspeed: {} m/s", 0));
+            &format!("speed: {} m/s ({} km/h)", glider.velocity, glider.velocity * 3.6));
         ui::root_ui().label(
             vec2(self.pos.x + 10., self.pos.y + 10. + hoffset), 
-            &format!("hspeed: {} m/s", 0));
-    }
-}
-
-struct DebugPanel {
-    pos: Vec2,
-    size: Vec2,
-}
-
-impl DebugPanel {
-    fn new(pos: Vec2, size: Vec2) -> Self {
-        Self { pos, size }
-    }
-
-    fn draw(&self, _glider: &Glider) {
-        drawx::draw_rounded_rectangle(self.pos, self.size, 10., PANEL_COLOR); 
-        
-        ui::root_ui().label(
-            vec2(self.pos.x + 10., self.pos.y + 10.), 
-            &format!("Info: "));
+            &format!("variometer: {} m/s", glider.hvelocity));
     }
 }
 
@@ -140,7 +200,14 @@ fn window_conf() -> Conf {
 async fn main() {
     // Sim
     let mut glider = Glider::new();
+    let flying_field = FlyingField::new();
     // UI
+    let glider_image = load_texture("assets/glider.png").await.unwrap();
+    let map = MapView::new(
+        vec2(0., 0.), 
+        vec2(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32 - 100.),
+        flying_field,
+        glider_image);
     let label_style = ui::root_ui()
         .style_builder()
         .font_size(LABEL_FONT_SIZE)
@@ -153,16 +220,13 @@ async fn main() {
 
     let position_panel = PositionPanel::new(
         vec2(5., WINDOW_HEIGHT as f32 - 100.),
-        vec2(200., 95.));
+        vec2(300., 95.));
     let orientation_panel = OrientationPanel::new(
-        vec2(210., WINDOW_HEIGHT as f32 - 100.),
-        vec2(200., 95.));
+        vec2(310., WINDOW_HEIGHT as f32 - 100.),
+        vec2(300., 95.));
     let velocity_panel = VelocityPanel::new(
-        vec2(415., WINDOW_HEIGHT as f32 - 100.),
-        vec2(200., 95.));
-    let debug_panel = DebugPanel::new(
-        vec2(620., WINDOW_HEIGHT as f32 - 100.),
-        vec2(WINDOW_WIDTH as f32 - 625., 95.));
+        vec2(615., WINDOW_HEIGHT as f32 - 100.),
+        vec2(WINDOW_WIDTH as f32 - 620., 95.));
 
     loop {
         let dt = get_frame_time();
@@ -174,13 +238,20 @@ async fn main() {
             break;
         }
 
+        if is_key_down(KeyCode::Left) {
+            glider.turn_left(dt);
+        }
+        if is_key_down(KeyCode::Right) {
+            glider.turn_right(dt);
+        }
         // Draw universe
         clear_background(BACKGROUND_COLOR);
         draw_rectangle(0., WINDOW_HEIGHT as f32 - 105., WINDOW_WIDTH as f32, 120., SIDEBAR_COLOR);
+
+        map.draw(&glider);
         orientation_panel.draw(&glider);
         position_panel.draw(&glider);
         velocity_panel.draw(&glider);
-        debug_panel.draw(&glider);
         
         next_frame().await
     }
